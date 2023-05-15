2a10d60cb5f8c9a71b3b3bd8934fe5e832278f38
decentraliser <me@decentraliser.dev>
2023-05-15 16:18:55 +0200

    bootstrap navigation menu

--- app.vue
+++ app.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NavigationMenu />
   </div>
 </template>
+
+<script setup>
+import { useDark } from "@vueuse/core";
+
+const isDark = useDark({
+  selector: "body", //element to add attribute to
+  attribute: "theme", // attribute name
+  valueDark: "custom-dark", // attribute value for dark mode
+  valueLight: "custom-light", // attribute value for light mode
+});
+</script>
+
+<style>
+[theme="custom-dark"] {
+  background: #16171d;
+  color: #fff;
+}
+</style>
---
+++ components/NavigationMenu.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>Home</div>
+    <div>The artist</div>
+    <div>Team</div>
+    <div>Gallery</div>
+    <div>Buy</div>
+    <div>More</div>
+    <div>References</div>
+    <div>License agreement</div>
+    <div>Terms & Conditions</div>
+    <div>Discord</div>
+    <div>Twitter</div>
+  </div>
+
+  <div>
+    <img
+      @click="toggleDark()"
+      v-if="isDark"
+      src="~/assets/images/lightMode.png"
+    />
+    <img
+      @click="toggleDark()"
+      v-if="!isDark"
+      src="~/assets/images/darkMode.png"
+    />
+  </div>
+</template>
+
+<script setup>
+import { useDark, useToggle } from "@vueuse/core";
+
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+</script>
