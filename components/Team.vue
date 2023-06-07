c7839bf1c40f1d8123c2267ef7ee90218f693f8a
decentraliser <me@decentraliser.dev>
2023-06-07 16:45:41 +0200

    add "The team" title, make members roles italic

--- components/Team.vue
+++ components/Team.vue
@@ -1,42 +1,49 @@
 <template>
   <section id="team">
-    <div class="team-member">
-      <img src="@/assets/images/lugosis.png" alt="Lugosis" />
-      <div class="team-member-name">Lugosis</div>
-      <div class="team-member-position">Artist</div>
-    </div>
+    <h1>The team</h1>
 
-    <div class="team-member">
-      <img src="@/assets/images/dan.png" alt="Dan" />
-      <div class="team-member-name">Dan</div>
-      <div class="team-member-position">Art curator</div>
-    </div>
+    <div class="team-members">
+      <div class="team-member">
+        <img src="@/assets/images/lugosis.png" alt="Lugosis" />
+        <div class="team-member-name">Lugosis</div>
+        <div class="team-member-position"><i>Artist</i></div>
+      </div>
 
-    <div class="team-member">
-      <img src="@/assets/images/victor.png" alt="Victor" />
-      <div class="team-member-name">Victor</div>
-      <div class="team-member-position">Project lead</div>
-    </div>
+      <div class="team-member">
+        <img src="@/assets/images/dan.png" alt="Dan" />
+        <div class="team-member-name">Dan</div>
+        <div class="team-member-position"><i>Art curator</i></div>
+      </div>
 
-    <div class="team-member">
-      <img src="@/assets/images/krillin.png" alt="Krillin" />
-      <div class="team-member-name">Krillin</div>
-      <div class="team-member-position">Incubator</div>
+      <div class="team-member">
+        <img src="@/assets/images/victor.png" alt="Victor" />
+        <div class="team-member-name">Victor</div>
+        <div class="team-member-position"><i>Project lead</i></div>
+      </div>
+
+      <div class="team-member">
+        <img src="@/assets/images/krillin.png" alt="Krillin" />
+        <div class="team-member-name">Krillin</div>
+        <div class="team-member-position"><i>Incubator</i></div>
+      </div>
     </div>
   </section>
 </template>
 
 <style>
 #team {
-  margin-top: 130px;
+  margin-top: 150px;
   position: relative;
+  margin-bottom: 50px;
+}
+
+.team-members {
   display: grid;
   width: 100%;
   grid-auto-flow: row;
   place-content: center;
   text-align: center;
   row-gap: 60px;
-  margin-bottom: 50px;
 }
 
 .team-member {
