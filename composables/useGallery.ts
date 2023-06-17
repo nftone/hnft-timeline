1493bcebfc62bb0b3f1a8b7b40165d106c3e19ac
decentraliser <me@decentraliser.dev>
2023-06-17 15:42:07 +0200

    add use gallery composable

---
+++ composables/useGallery.ts
@@ -0,0 +1,29 @@
+import { computed, ref } from "vue";
+import Categories from "~/constants/Categories";
+import fakeAsset from "~/constants/fakeAsset";
+
+type UseGallery = {
+  categories: typeof Categories;
+  displayedAssets: ComputedRef<Asset[]>;
+  filters: Ref<string[]>;
+};
+
+const filters = ref([]);
+
+export default function useGallery(): UseGallery {
+  const assets = Array.from({ length: 10 }, () => fakeAsset);
+
+  const displayedAssets = computed(() => {
+    console.log("aa");
+    if (!filters.value.length) return assets;
+    return assets.filter(({ traits }) =>
+      filters.value.some((trait) => traits.includes(trait))
+    );
+  });
+
+  return {
+    categories: Categories,
+    displayedAssets,
+    filters,
+  };
+}
