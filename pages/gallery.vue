ed1d188036533b18eb93795cfac9e4f29fb97249
decentraliser <me@decentraliser.dev>
2023-06-17 15:42:29 +0200

    implement gallery mobile view

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,1 +1,43 @@
-<template>Gallery</template>
+<template>
+  <div class="gallery-container">
+    <div class="assets-container">
+      <div
+        class="asset-container"
+        v-for="(asset, i) in displayedAssets"
+        :key="`asset-${i}-${asset.tokenId}`"
+      >
+        <img :src="`/images/${asset.image}`" :alt="asset.image" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import useGallery from "@/composables/useGallery";
+const { metadata: AssetsMetadata, displayedAssets, filters } = useGallery();
+console.log("🚀 ~ file: gallery.vue:6 ~ displayedAssets:", displayedAssets);
+</script>
+
+<style>
+.gallery-container {
+  margin-top: 154px;
+  padding: 30px;
+}
+
+.assets-container {
+  display: grid;
+  row-gap: 16px;
+}
+
+.asset-container {
+  display: grid;
+  place-content: center;
+  width: 100%;
+  overflow: hidden;
+}
+
+.asset-container img {
+  /* max-width: 260px; */
+  width: 100%;
+}
+</style>
