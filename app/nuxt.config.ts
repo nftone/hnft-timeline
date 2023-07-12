face681cb2891ef66c9e93c7d0f62af052fe6980
decentraliser <me@decentraliser.dev>
2023-07-12 11:53:00 +0200

    remove baseUrl

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -2,12 +2,12 @@
 export default defineNuxtConfig({
   ssr: false,
   app: {
-    baseURL: "/hnft-timeline/",
+    baseURL: "/",
     head: {
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
       title: "Historical Non-Fungible Tokens Timeline",
     },
   },
-  modules: ["@vueuse/nuxt", '@nuxt/image'],
+  modules: ["@vueuse/nuxt", "@nuxt/image"],
 });
