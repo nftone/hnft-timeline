71ae0003cf27ba3e8e957df1c2049c4894671c06
decentraliser <me@decentraliser.dev>
2023-09-06 11:33:00 +0700

    update user-message route

--- app/constants/appConstants.ts
+++ app/constants/appConstants.ts
@@ -1,2 +1,2 @@
-export const USER_MESSAGE_URL = "https://api.hnft.wtf/user-message"
+export const USER_MESSAGE_URL = "https://api.explorer.hnft.wtf/user-message"
 // export const USER_MESSAGE_URL = "http://localhost:5556/user-message"
